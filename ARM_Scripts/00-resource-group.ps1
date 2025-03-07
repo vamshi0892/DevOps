2ee0677c041ab727441e1bc6e6c7f2382aaf5d9a
vamshi0892 <vamshi0892@gmail.com>
2025-03-07 15:07:57 -0500

    Updated script

--- ARM_Scripts/00-resource-group.ps1
+++ ARM_Scripts/00-resource-group.ps1
@@ -7,5 +7,5 @@ New-AzResourceGroup -Name VamshiResourceGroup2 -Location "East US"
 
 New-AzResourceGroupDeployment `
   -Name blanktemplate `
-  -ResourceGroupName myResourceGroup `
+  -ResourceGroupName VamshiResourceGroup2 `
   -TemplateFile $templateFile
