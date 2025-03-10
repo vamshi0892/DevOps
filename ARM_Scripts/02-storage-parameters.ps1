4ed2f2ba5b8150d89e5026cf2b801da1706ad430
vamshi0892 <vamshi0892@gmail.com>
2025-03-09 21:44:01 -0400

    param changes

--- ARM_Scripts/02-storage-parameters.ps1
+++ ARM_Scripts/02-storage-parameters.ps1
@@ -1,9 +1,11 @@
 $templateFile = "C:\Users\vamsh\OneDrive\Desktop\DevOps\ARM_Templates\02-storage-parameters.json"
 $rg = "VamshiResourceGroup"
 $storage_Name = "vamshiblobadlsgen99"
+$storake_SKU = "Standard_LRS"
 
 New-AzResourceGroupDeployment `
     -Name addStorage `
     -ResourceGroupName $rg `
     -templateFile $templateFile `
-    -storageName $storage_Name
+    -storageName $storage_Name `
+    -storageSKU $storake_SKU
